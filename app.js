6807fd808cccdba331f4726e042d1e8fc55580f5
François GERBES <francois.gerbes@sb-roscoff.fr>
2019-05-24 13:02:42 +0200

    try to have more log when NODE_DEBUG is set

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var http = require('http');
 var session = require('express-session');
 
 var log_level = 'info';
-if (process.env.NODE_ENV == 'dev') {
+if (process.env.NODE_ENV == 'dev' || process.env.NODE_DEBUG) {
     log_level = 'debug';
 }
 
@@ -19,7 +19,8 @@ var winston = require('winston');
 var jwt = require('jsonwebtoken');
 
 const myconsole = new (winston.transports.Console)({
-  label: 'gomngr'
+  label: 'gomngr',
+  level: log_level
 });
 winston.loggers.add('gomngr', {
     transports: [myconsole]
@@ -304,7 +305,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') === 'development' || process.env.NODE_DEBUG) {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
@@ -313,17 +314,17 @@ if (app.get('env') === 'development') {
         });
     });
 }
-
+else {
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
+  app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+          message: err.message,
+          error: {}
+      });
+  });
+}
 
 module.exports = app;
 
