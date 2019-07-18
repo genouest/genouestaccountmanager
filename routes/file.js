21cadf243dbaedbf0b0cb43a5ab0780a2fdfa40b
Fanch <francois.gerbes@sb-roscoff.fr>
2019-07-18 15:53:24 +0200

    add file template management + one example

---
+++ routes/file.js
@@ -0,0 +1,73 @@
+const nunjucks = require('nunjucks');
+const winston = require('winston');
+const logger = winston.loggers.get('gomngr');
+var CONFIG = require('config');
+var fs = require('fs');
+
+// Todo: move utils function which manage file content here
+// var utils = require('../routes/utils.js');
+
+// Todo use conf for template directory
+nunjucks.configure('templates', { autoescape: true });
+
+// Todo, move this in config file
+const tplconf = {
+    ssh_config: {
+        filename: "config.test",
+        filenamemode: 0o600,
+        filepath: "{{ user.home }}/.ssh",
+        filepathmode: 0o700,
+        template: "ssh_config",
+    }
+};
+
+
+/* Example Usage */
+
+/*
+file.create_ssh_config(user)
+    .then(
+        data => { // resolve()
+            logger.info('File created: ', data);
+        })
+    .catch(error => { // reject()
+        logger.error('File not created: ', error);
+    });
+*/
+
+
+function create_file (name, data) {
+    return new Promise( function (resolve, reject) {
+        const tpl = tplconf[name];
+
+        nunjucks.renderString(tpl.filepath, data, function (err, filepath) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            fs.mkdirSync(filepath, { recursive: true });
+            fs.chmodSync(filepath, tpl.filepathmode);
+
+            nunjucks.render(tpl.template, data, function (err, content) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                // todo find if we need to render filename too
+                fs.writeFileSync(filepath + "/" + tpl.filename, content);
+                fs.chmodSync(filepath + "/" + tpl.filename, tpl.filenamemode);
+                resolve (filepath + "/" + tpl.filename);
+                return;
+            });
+        });
+    });
+}
+
+
+module.exports = {
+    create_ssh_config: function (user) {
+        return create_file('ssh_config', { user: user });
+    }
+};
