98e9830da824d89c7b1c178dd0b1299a0acaf1f4
Fanch <francois.gerbes@sb-roscoff.fr>
2019-07-26 11:00:14 +0200

    [4] Find ldap cn from uid  (#50)
    * try again with more clean change
    
    * add a reject if no user found

--- import_from_ldap.js
+++ import_from_ldap.js
@@ -136,8 +136,8 @@ function search_users(){
 function record_user(user){
     console.debug(user);
 
-    if(! user.dn.startsWith("uid=")){
-        console.warn("[SKIP] Invalid DN for user ", user.uid, ",DN= ", user.dn, ", should be uid=...");
+    if(user.uid || user.uid == ""){
+        console.warn("[SKIP] Invalid DN for user ", user.uid, ",DN= ", user.dn, ", must have be uid=...");
         return;
     }
 
