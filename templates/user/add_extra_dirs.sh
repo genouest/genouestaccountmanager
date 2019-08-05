a9ffe8eb4b88c48fd4047e842944bc72bce7979c
Fanch <francois.gerbes@sb-roscoff.fr>
2019-08-05 11:42:52 +0200

    try to create extradir using old #USER#

--- templates/user/add_extra_dirs.sh
+++ templates/user/add_extra_dirs.sh
@@ -1,2 +1,10 @@
-# todo find if we need to add user extra dirs or not (as it not used in default and test config)
-# todo if we need to add extra dirs, find a clean way to use user.home and user.group
+# todo find a clean way to use user and group
+
+{% for item in CONFIG.general.user_extra_dirs %}
+if [ ! -e "{{ item | replace("#USER#", user.uid) | replace ("#GROUP#", user.group) }}" ]
+then
+    mkdir -p "{{ item | replace("#USER#", user.uid)| replace ("#GROUP#", user.group) }}"
+    chown -R {{ user.uidnumber }}:{{ user.gidnumber }} "{{ item | replace("#USER#", user.uid)| replace ("#GROUP#", user.group) }}"
+fi;
+
+{% endfor %}
