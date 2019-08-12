3e548b67338e0c003a62a81bcb4822a9a8528728
Fanch <francois.gerbes@sb-roscoff.fr>
2019-08-12 10:05:18 +0200

    remove \n for templates shebang

--- templates/user/delete_user.sh
+++ templates/user/delete_user.sh
@@ -1,4 +1,4 @@
-#!/bin/bash\n";
+#!/bin/bash
 
 #set -e
 ldapmodify -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
