4e9fc1e7288adef8c0b2e7414aaf91f72a40cbe5
Fanch <francois.gerbes@sb-roscoff.fr>
2019-08-28 17:09:31 +0200

    add a a call to add group to project with num in ifb template

--- templates/ifb/project/add_group_to_project.sh
+++ templates/ifb/project/add_group_to_project.sh
@@ -2,5 +2,7 @@
 
 set -xe
 
-echo "should do something or not with {{ project.id }} and {{ group.name }}"
+# todo: add a call to this template in my code, or not
+/usr/local/bin/num add-group-to-project --project={{ project.id }} --group={{ group.name }}
+
 # add_group_to_project.sh
--- templates/ifb/project/add_project.sh
+++ templates/ifb/project/add_project.sh
@@ -4,4 +4,7 @@ set -xe
 
 /usr/local/bin/num create-project {{ project.id }}
 
+#TODO: find if it is a good idea to do this here
+/usr/local/bin/num add-group-to-project --project={{ project.id }} --group={{ project.group }}
+
 # add_project.sh
