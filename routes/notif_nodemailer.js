84e5c7c22a3cf91ed4f9d49e7fd17813a93e6639
Fanch <francois.gerbes@sb-roscoff.fr>
2019-09-03 10:54:12 +0200

    [3] Add mailer management (#66)
    * Rename notif to notif_gomail and get the mailer from config
    
    * add mailer config in example conf + test conf
    
    * use notif_gomail.js in gomail plugins
    
    * use notif_gomail in expiration script
    
    * remove duplicate require notif
    
    * add empty nodemailer notif script
    
    * send true to every call back
    
    * add default nodemailer config
    
    * ugly fix missing origin from mailer conf
    
    * ugly fix missing mailer conf
    
    * add smtp config to nodemailer
    
    * add nodemailer package as dep
    
    * Try to send email with nodemailer
    
    * update log message on sent mail
    
    * add missing coma in config
    
    * bugfix log message on smtp sent mail

---
+++ routes/notif_nodemailer.js
@@ -0,0 +1,102 @@
+const CONFIG = require('config');
+const fs = require('fs');
+const monk = require('monk'),
+      db = monk(CONFIG.mongo.host+':'+CONFIG.mongo.port+'/'+CONFIG.general.db),
+      web_db = db.get('web'),
+      users_db = db.get('users'),
+      events_db = db.get('events');
+const winston = require('winston');
+const logger = winston.loggers.get('gomngr');
+const request = require('request');
+const nodemailer = require("nodemailer");
+
+var mail_set = false;
+// mailset should be a isnull on transporter (or not)
+var transporter = null;
+
+if(CONFIG.nodemailer){
+    transporter = nodemailer.createTransport({
+        host: CONFIG.nodemailer.host,
+        port: CONFIG.nodemailer.port,
+        secure: false, // upgrade later with STARTTLS
+    });
+
+    transporter.verify(function(error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            mail_set =true;
+            console.log("Smtp Server is ready to take our messages");
+        }
+    });
+}
+
+module.exports = {
+
+    mailSet: function(){
+        return mail_set;
+    },
+
+    subscribed: function(email, callback) {
+        logger.warn("subscribed: no mailing list configured", email);
+        callback(false);
+        return;
+    },
+
+    getMembers: function(list, callback) {
+        logger.warn("getMembers: no mailing list configured", list);
+        callback(false);
+        return;
+    },
+
+    getLists: function(callback){
+        logger.warn("getLists: no mailing list configured");
+        callback([]);
+        return;
+    },
+
+    add: function(email, callback) {
+        logger.warn("add: no mailing list configured", email);
+        callback();
+        return;
+    },
+
+    create: function(name, callback) {
+        logger.warn("create: no mailing list configured", name);
+        callback();
+        return;
+    },
+
+    remove: function(email, callback) {
+        logger.warn("remove: no mailing list configured", email);
+        callback();
+        return;
+    },
+
+    modify: function(oldemail, newemail, callback) {
+        logger.warn("remove: no mailing list configured", oldemail, newemail);
+        callback();
+        return;
+    },
+
+    sendUser: function(mailOptions, callback) {
+
+        let info =  transporter.sendMail({
+            from: mailOptions.origin,
+            to: mailOptions.destinations.join(),
+            subject: mailOptions.subject,
+            html: mailOptions.html_message
+        });
+
+        logger.info("Message sent to " + mailOptions.destinations.join() + ":" + mailOptions.subject);
+
+        callback("" , true);
+        return;
+    },
+
+    sendList: function(mailing_list, mailOptions, callback) {
+        logger.warn("sendList: no mailing list configured", mailing_list, mailOptions);
+        callback("no mailing list configured", true);
+        return;
+    }
+};
