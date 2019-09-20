3a2a5694847974c6b23bcf295f7905de1480bcfb
Fanch <francois.gerbes@sb-roscoff.fr>
2019-09-20 14:57:42 +0200

    Save subscribed status for nodemailer notif (#89)
    * try to save subscribed status
    
    * try to fix unhandle promise callback
    
    * try (again) to fix unhandle promise callback

--- routes/notif_nodemailer.js
+++ routes/notif_nodemailer.js
@@ -38,8 +38,17 @@ module.exports = {
     },
 
     subscribed: function(email, callback) {
-        logger.warn("subscribed: no mailing list configured", email);
-        callback(false);
+        if (CONFIG.nodemailer.list) {
+            users_db.findOne({email: email}, function(err, user){
+                if(!user) {
+                    logger.error('User does not exist', email);
+                    callback(false);
+                    return;
+                }
+                callback(user.subscribed);
+                return;
+            });
+        }
         return;
     },
 
@@ -56,20 +65,31 @@ module.exports = {
     },
 
     add: function(email, callback) {
-        if(email==undefined ||email==null || email=='' || ! mail_set) {
-            callback();
-            return;
-        }
         if (CONFIG.nodemailer.list) {
-            transporter.sendMail({
-                from: CONFIG.nodemailer.origin,
-                to: CONFIG.nodemailer.list.cmd_address,
-                subject: CONFIG.nodemailer.list.cmd_add + ' ' + CONFIG.nodemailer.list.address + ' ' + email
+            if(email==undefined ||email==null || email=='' || ! mail_set) {
+                callback();
+                return;
+            }
+            users_db.findOne({email: email}, function(err, user){
+                if(!user) {
+                    logger.error('User does not exist', email);
+                    callback();
+                    return;
+                }
+                transporter.sendMail({
+                    from: CONFIG.nodemailer.origin,
+                    to: CONFIG.nodemailer.list.cmd_address,
+                    subject: CONFIG.nodemailer.list.cmd_add + ' ' + CONFIG.nodemailer.list.address + ' ' + email
+                });
+                logger.info("user added to " + CONFIG.nodemailer.list.address, email);
+
+                events_db.insert({'date': new Date().getTime(), 'action': 'add ' + email + 'to mailing list' , 'logs': []}, function(err){});
+
+                users_db.update({email: email}, {'$set':{'subscribed': true}}, function(err, data){});
+
             });
-            logger.info("user added to " + CONFIG.nodemailer.list.address, email);
         }
         callback();
-        events_db.insert({'date': new Date().getTime(), 'action': 'add ' + email + 'to mailing list' , 'logs': []}, function(err){});
         return;
     },
 
@@ -79,20 +99,32 @@ module.exports = {
         return;
     },
 
+    // todo: should be factorized with add, as there is only small difference
     remove: function(email, callback) {
-        if(email==undefined ||email==null || email=='' || ! mail_set) {
-            callback();
-            return;
-        }
         if (CONFIG.nodemailer.list) {
-            transporter.sendMail({
-                from: CONFIG.nodemailer.origin,
-                to: CONFIG.nodemailer.list.cmd_address,
-                subject: CONFIG.nodemailer.list.cmd_del + ' ' + CONFIG.nodemailer.list.address + ' ' + email
+            if(email==undefined ||email==null || email=='' || ! mail_set) {
+                callback();
+                return;
+            }
+            users_db.findOne({email: email}, function(err, user){
+                if(!user) {
+                    logger.error('User does not exist', email);
+                    callback();
+                    return;
+                }
+                transporter.sendMail({
+                    from: CONFIG.nodemailer.origin,
+                    to: CONFIG.nodemailer.list.cmd_address,
+                    subject: CONFIG.nodemailer.list.cmd_del + ' ' + CONFIG.nodemailer.list.address + ' ' + email
+                });
+                logger.warn("user deleted from " + CONFIG.nodemailer.list.address, email);
+
+                events_db.insert({'date': new Date().getTime(), 'action': 'unsubscribe ' + email + ' from mailing list' , 'logs': []}, function(err){});
+
+                users_db.update({email: email}, {'$set':{'subscribed': false}}, function(err, data){});
+
             });
-            logger.warn("user deleted from " + CONFIG.nodemailer.list.address, email);
         }
-        events_db.insert({'date': new Date().getTime(), 'action': 'unsubscribe ' + email + ' from mailing list' , 'logs': []}, function(err){});
         callback();
         return;
     },
