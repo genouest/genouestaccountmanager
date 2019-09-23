0068f6c0f14b33adbf42319c7f4101b6eda786ef
Fanch <francois.gerbes@sb-roscoff.fr>
2019-09-23 14:21:15 +0200

    add missing map import

--- manager2/src/app/user/user.service.ts
+++ manager2/src/app/user/user.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { AuthService } from '../auth/auth.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
