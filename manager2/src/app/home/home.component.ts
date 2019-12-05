2c8b71047f5dff0ad38a286b42c01c54dd998c3f
Fanch <francois.gerbes@sb-roscoff.fr>
2019-12-05 17:58:07 +0100

    add config service in home component

--- manager2/src/app/home/home.component.ts
+++ manager2/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
+import { ConfigService } from '../config.service'
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,9 +10,19 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-    constructor(private authService: AuthService, private router: Router) { }
+    config: any
+
+    constructor(private authService: AuthService,
+                private configService: ConfigService,
+                private router: Router) {
+        this.config = {}
+    }
 
     ngOnInit() {
+        this.configService.config.subscribe(
+            resp => this.config = resp,
+            err => console.log('failed to get config')
+        )
         if (this.authService.isLoggedIn) {
             let user = this.authService.profile;
             this.router.navigate(['/user/' + user.uid]);
