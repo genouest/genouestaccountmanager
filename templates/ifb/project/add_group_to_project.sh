4a99bb7388197ba2d8cf260bf06bffd1b30be30e
Fanch <francois.gerbes@sb-roscoff.fr>
2019-12-12 15:52:01 +0100

    remove call to num for add group to project (#137)

--- templates/ifb/project/add_group_to_project.sh
+++ templates/ifb/project/add_group_to_project.sh
@@ -3,6 +3,6 @@
 set -xe
 
 # todo: add a call to this template in my code, or not
-/usr/local/bin/num add-group-to-project {{ group.name }} {{ project.id }}
+# /usr/local/bin/num add-group-to-project {{ group.name }} {{ project.id }}
 
 # add_group_to_project.sh
