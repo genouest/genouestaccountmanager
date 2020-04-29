66f27bd7ae825cd057f12776748318eee7f328c2
Fanch <francois.gerbes@sb-roscoff.fr>
2020-04-29 20:47:42 +0200

    check if value is undefinned or null

--- routes/utils.js
+++ routes/utils.js
@@ -477,7 +477,7 @@ async function gen_mail_opt (options, variables)
     // replace variable in message
     for (let key in variables) {
         let value = variables[key];
-        if (!value) { value = '';} // value may not be defined
+        if (value === undefined || value === null) { value = '';} // value may not be defined
         let html_value = value;
         let re = new RegExp(key,'g');
 
