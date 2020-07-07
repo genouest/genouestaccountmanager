31664910a7665140d0cd4e926e0fd60943ca49db
Fanch <fanch.gerbes@gmail.com>
2020-07-07 15:08:23 +0200

    add a try catch and a default if config key is missing

--- routes/users.js
+++ routes/users.js
@@ -2071,8 +2071,13 @@ var add_user_to_project = async function (newproject, uid, action_owner) {
         logger.error(error);
     }
 
-    if (CONFIG.project.enable_group) {
-        add_user_to_group(project.group, user.uid, action_owner);
+    if (CONFIG.project === undefined || CONFIG.project.enable_group) {
+        try {
+            await add_user_to_group(project.group, user.uid, action_owner);
+        } catch (error) {
+            logger.error(error);
+            throw {code: 500, msg:'Add User to group;' + project.group + ' Failed for project: ' + project.id};
+        }
     }
 };
 
