eb29d7db0bbe31c9177ce3b4ba14de5189c7a59c
KonoganB <konogan.bourhy@gmail.com>
2020-11-17 14:53:54 +0100

    remove

--- routes/projects.js
+++ routes/projects.js
@@ -621,15 +621,15 @@ router.post('/dmp/download', async function (req, res) {
 
 router.get('/pending', async function (req, res) {
     
-    if (!req.locals.logInfo.is_logged) {
-        res.status(401).send('Not authorized');
-        return;
-    }
-    let user = await utils.mongo_users().findOne({ _id: req.locals.logInfo.id });
-    if (!user) {
-        res.status(404).send('User not found');
-        return;
-    }
+    // if (!req.locals.logInfo.is_logged) {
+    //     res.status(401).send('Not authorized');
+    //     return;
+    // }
+    // let user = await utils.mongo_users().findOne({ _id: req.locals.logInfo.id });
+    // if (!user) {
+    //     res.status(404).send('User not found');
+    //     return;
+    // }
     // if (GENERAL_CONFIG.admin.indexOf(user.uid) < 0) {
     //     if (!user.pending) {
     //         res.send([]);
