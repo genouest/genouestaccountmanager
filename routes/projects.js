1ade7ee145dbd11060cd86a4d7ec68548cedcddc
KonoganB <konogan.bourhy@gmail.com>
2020-11-19 11:19:22 +0100

    save in mongo pending

--- routes/projects.js
+++ routes/projects.js
@@ -438,6 +438,7 @@ router.post('/ask/project', async function (req, res) {
         res.status(404).send('User not found');
         return;
     }
+    
 
     /* // New Project Structure :
       let new_project = {
@@ -449,14 +450,16 @@ router.post('/ask/project', async function (req, res) {
       */
 
     // todo: find a way to use cc
-    // let new_project = {
-    //     'id': req.body.id,
-    //     'size': req.body.size,
-    //     'description': req.body.description,
-    //     'orga': req.body.orga
-    // };
+    let new_project = {
+        'id': req.body.id,
+        'owner': req.locals.logInfo.id,
+        'group': req.locals.logInfo.group,
+        'size': req.body.size,
+        'description': req.body.description,
+        'orga': req.body.orga
+    };
     // Save in mongo the pending project data fr the admin to use
-    // let saving_for_later = await utils.mongo_projects().insertOne(new_project);
+    let saving_for_later = await utils.mongo_pending().insertOne(new_project);
 
     let msg_destinations = [GENERAL_CONFIG.accounts, user.email];
     try {
@@ -619,7 +622,7 @@ router.post('/dmp/download', async function (req, res) {
 });
 
 router.get('/pending', async function (req, res) {
-    
+
     if (!req.locals.logInfo.is_logged) {
         res.status(401).send('Not authorized');
         return;
