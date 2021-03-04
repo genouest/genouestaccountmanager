8df549bd071f88e77f89a3328e3a14d8741e63d9
Fanch <fanch@libre.bzh>
2021-03-04 17:23:27 +0100

    split utils file in service

---
+++ core/plugin.service.js
@@ -0,0 +1,31 @@
+var Promise = require('promise');
+const winston = require('winston');
+const logger = winston.loggers.get('gomngr');
+var CONFIG = require('config');
+
+var plugins = CONFIG.plugins;
+if(plugins === undefined){
+    plugins = [];
+}
+var plugins_modules = {};
+var plugins_info = [];
+
+exports.load_plugins = () => {
+    for(let i=0;i<plugins.length;i++){
+        if(!plugins_modules[plugins[i].name]) {
+            plugins_modules[plugins[i].name] = require('../plugins/'+plugins[i].name);
+            if(plugins[i].display_name === undefined) { plugins[i]['display_name'] = plugins[i].name; }
+            if(plugins[i].admin_only === undefined) { plugins[i]['admin_only'] = false; }
+            if(plugins[i].admin === undefined) { plugins[i]['admin'] = false; }
+            plugins_info.push({'name': plugins[i].name, 'url': '../plugin/' + plugins[i].name, 'display_name': plugins[i]['display_name'], 'admin_only': plugins[i]['admin_only'], 'admin': plugins[i]['admin']});
+        }
+    }
+};
+
+exports.plugins_info = () => {
+    return plugins_info;
+};
+
+exports.plugins_modules = () => {
+    return plugins_modules;
+};
---
+++ core/sanitize.service.js
@@ -0,0 +1,46 @@
+/* TODO : SHOULD SPLIT THIS FILE, one for mongo, one for redis, on for mail ..... */
+
+var Promise = require('promise');
+const winston = require('winston');
+const logger = winston.loggers.get('gomngr');
+var CONFIG = require('config');
+
+function sanitizeString(rawValue) {
+    if (typeof rawValue === 'string' && /^[0-9a-z-_]+$/i.test(rawValue)) {
+        return rawValue;
+    }
+    return undefined;
+}
+
+exports.sanitizeSSHKey = function(rawValue) {
+    if (typeof rawValue === 'string' && /^ssh-rsa AAAA[0-9A-Za-z+/]+[=]{0,3}/.test(rawValue)) {
+        return rawValue;
+    }
+    return undefined;
+};
+
+exports.sanitizePath = function(rawValue) {
+    // eslint-disable-next-line no-useless-escape
+    if (typeof rawValue === 'string' && /^[0-9a-z-_\s\/.]+$/i.test(rawValue)) {
+        return rawValue;
+    }
+    return undefined;
+};
+
+exports.sanitize = function sanitize(rawValue) {
+    let value = sanitizeString(rawValue);
+    if (value == undefined) {
+        return false;
+    }
+    return true;
+};
+
+exports.sanitizeAll = function sanitizeAll(rawValues) {
+    for(let i=0;i<rawValues.length;i++) {
+        let value = sanitizeString(rawValues[i]);
+        if (value === undefined) {
+            return false;
+        }
+    }
+    return true;
+};
