2f5ed5d62b65f4c48618688e4fb83d4d14ad282b
Fanch <fanch@libre.bzh>
2021-07-05 17:29:25 +0200

    use bootstrap class

--- manager2/src/app/info/RegisteredInfoComponent.ts
+++ manager2/src/app/info/RegisteredInfoComponent.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-info',
-    template: `<p>Your account creation has been taken into account.</p>
+    template: `<div class="alert alert-info">
+<p class="alert alert-success">Your account creation has been taken into account.</p>
 <br>
 <p>You will receive an email soon to <b>confirm your email address</b>.</p>
-<p style='color: red'><b> Please check your email and your spam folder ! </b></p>
-<br>
+<p class="alert alert-warning"><b> Please check your email and your spam folder ! </b></p>
 <p>Once confirmed, administrators will <b>manually validate</b> your account.</p>
+</div>
 `,
     // templateUrl: './info.component.html',
     styleUrls: ['./info.component.css']
