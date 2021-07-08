a3059f49e126431b16bf9fbbe02fcbd3603f074e
Gildas Le Corguillé <lecorguille@users.noreply.github.com>
2021-07-08 11:43:16 +0200

    [abims] Fix quota

--- templates/abims/project/add_project.sh
+++ templates/abims/project/add_project.sh
@@ -4,6 +4,8 @@ set -xe
 
 /usr/local/bin/num create-project {{ project.id }}
 
-/usr/local/bin/num set-quota -sq {{ project.size }}GB {{ project.path }}
+# Disable because it's not the same command to create a quota and modify/apply it
+# @TODO add template in num to allow modifying quota
+#/usr/local/bin/num set-quota -sq {{ project.size }}GB {{ project.path }}
 
 # add_project.sh
