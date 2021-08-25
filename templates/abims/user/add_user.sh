bef2e5675e81bbc62af6d787106f32bc5d75b330
Fanch <fanch@libre.bzh>
2021-08-25 17:30:22 +0200

    test if email exist flag can be used in templates

--- templates/abims/user/add_user.sh
+++ templates/abims/user/add_user.sh
@@ -13,6 +13,11 @@ then
     ldapmodify -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/group_{{ user.group }}_{{ user.uid }}.{{ fid }}.ldif"
 fi
 
+{% if user.email_does_not_exist %}
+echo "should create mailbox"
+echo "should create aliases"
+{% endif %}
+
 {% include "user/add_readme.sh" %}
 
 mkdir -p "{{ user.home }}/.ssh"
@@ -23,8 +28,6 @@ echo '  UserKnownHostsFile=/dev/null' >> "{{ user.home }}/.ssh/config"
 chown -R {{ user.uid }}:{{ user.uid }} "{{ user.home }}"
 
 {% include "user/add_extra_dirs.sh" %}
-
-
 
 
 
