9a22a993d7fe08fcbc59f9f4d7f5a2ba7de1a4d0
Fanch <francois.gerbes@sb-roscoff.fr>
2021-09-02 11:04:01 +0200

    create mail at the end of the process

--- templates/abims/user/add_user.sh
+++ templates/abims/user/add_user.sh
@@ -13,11 +13,6 @@ then
     ldapmodify -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/group_{{ user.group }}_{{ user.uid }}.{{ fid }}.ldif"
 fi
 
-{% if user.create_imap_mailbox %}
-mel create-user-aliases "{{ user.uid }}"
-mel create-mailbox "{{ user.uid }}"
-{% endif %}
-
 {% include "user/add_readme.sh" %}
 
 mkdir -p "{{ user.home }}/.ssh"
@@ -29,6 +24,9 @@ chown -R {{ user.uid }}:{{ user.uid }} "{{ user.home }}"
 
 {% include "user/add_extra_dirs.sh" %}
 
-
+{% if user.create_imap_mailbox %}
+mel create-user-aliases "{{ user.uid }}"
+mel create-mailbox "{{ user.uid }}"
+{% endif %}
 
 # add_user.sh
