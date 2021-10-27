eb1db1995ebc051f925725d16440208e35f3faf4
Fanch <francois.gerbes@sb-roscoff.fr>
2021-10-27 11:49:14 +0200

    remove user group from abims and ifb template on user creation

--- templates/ifb/user/add_user.sh
+++ templates/ifb/user/add_user.sh
@@ -4,15 +4,6 @@ set -xe
 
 /usr/local/bin/num create-user --firstname="{{ user.firstname }}" --lastname="{{ user.lastname }}" --email="{{ user.email }}" --username="{{ user.uid }}" {% if user.home %}--home_dir="{{ user.home }}"{% endif %} {% if user.password %}--password="{{ user.password }}"{% endif %} {% if user.is_fake %}--service{% endif %}
 
-# warning: disable ldap as it should have been done by num, but we don't know what is the dn created yet
-
-#ldapadd -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
-
-if [ -e "{{ CONFIG.general.script_dir }}/group_{{ user.group }}_{{ user.uid }}.{{ fid }}.ldif" ]
-then
-    ldapmodify -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/group_{{ user.group }}_{{ user.uid }}.{{ fid }}.ldif"
-fi
-
 {% include "user/add_readme.sh" %}
 
 mkdir -p "{{ user.home }}/.ssh"
@@ -23,9 +14,5 @@ echo '  UserKnownHostsFile=/dev/null' >> "{{ user.home }}/.ssh/config"
 chown -R {{ user.uid }}:{{ user.uid }} "{{ user.home }}"
 
 {% include "user/add_extra_dirs.sh" %}
-
-
-
-
 
 # add_user.sh
