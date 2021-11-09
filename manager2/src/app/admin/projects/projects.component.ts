6c95cae779b05a3784a7125ff05e8c3070ebf8dd
Fanch <francois.gerbes@sb-roscoff.fr>
2021-11-09 15:24:51 +0100

    add owner of project and group on creation

--- manager2/src/app/admin/projects/projects.component.ts
+++ manager2/src/app/admin/projects/projects.component.ts
@@ -6,7 +6,7 @@ import { GroupsService } from 'src/app/admin/groups/groups.service';
 import { UserService } from 'src/app/user/user.service';
 import * as latinize from 'latinize'
 
-import {Table} from 'primeng/table'
+import { Table } from 'primeng/table'
 
 
 @Component({
@@ -63,7 +63,7 @@ export class ProjectsComponent implements OnInit {
     ngOnInit() {
         this.route.queryParams
             .subscribe(params => {
-                if(params.deleted == "ok") {
+                if (params.deleted == "ok") {
                     this.notification = "Project was deleted successfully";
                 };
             });
@@ -95,7 +95,7 @@ export class ProjectsComponent implements OnInit {
             resp => {
                 this.groups = resp;
                 if (this.groups.length > 0) {
-                    this.new_project.group =this.groups[0].name;
+                    this.new_project.group = this.groups[0].name;
                 }
             },
             err => console.log('failed to get groups')
@@ -131,8 +131,8 @@ export class ProjectsComponent implements OnInit {
 
 
     update_project_on_event(new_value) {
-        let tmpprojectid = latinize(new_value.toLowerCase()).replace(/[^0-9a-z]+/gi,'_');
-        this.new_project.path = this.config.project.default_path + '/' +  tmpprojectid;
+        let tmpprojectid = latinize(new_value.toLowerCase()).replace(/[^0-9a-z]+/gi, '_');
+        this.new_project.path = this.config.project.default_path + '/' + tmpprojectid;
         // warning: for this.new_project.id, (ngModelChange) must be after [ngModel] in html line
         // about order, see: https://medium.com/@lukaonik/how-to-fix-the-previous-ngmodelchange-previous-value-in-angular-6c2838c3407d
         this.new_project.id = tmpprojectid; // todo: maybe add an option to enable or disable this one
@@ -151,11 +151,11 @@ export class ProjectsComponent implements OnInit {
     }
 
 
-    add_project(){
+    add_project() {
         this.notification = "";
 
-        if(! this.new_project.id || (this.config.project.enable_group && ! this.new_project.group) || ! this.new_project.owner) {
-            this.add_project_error_msg = "Project Id, group, and owner are required fields " + this.new_project.id + this.new_project.group + this.new_project.owner ;
+        if (!this.new_project.id || (this.config.project.enable_group && !this.new_project.group) || !this.new_project.owner) {
+            this.add_project_error_msg = "Project Id, group, and owner are required fields " + this.new_project.id + this.new_project.group + this.new_project.owner;
             return;
         }
         this.reset_msgs()
@@ -170,39 +170,29 @@ export class ProjectsComponent implements OnInit {
             'access': this.new_project.access,
             'orga': this.new_project.orga,
             'path': this.new_project.path,
-            'expire': new Date(this.new_project.expire).getTime()}
-                               ).subscribe(
-                                   resp => {
-                                       this.add_project_msg = resp.message;
-                                       this.project_list();
-                                       this.pending_list(true);
-                                       this.userService.addToProject(this.new_project.owner, this.new_project.id).subscribe(
-                                           resp => {
-                                               this.new_project = {};
-                                           },
-                                           err => {
-                                               console.log('failed  to add user to project');
-                                               this.add_project_error_msg = err.error.message;
-                                           }
-                                       )
-
-                                   },
-                                   err => {
-                                       console.log('failed to add project', this.new_project);
-                                       this.add_project_error_msg = err.error.message;
-                                   }
-                               );
+            'expire': new Date(this.new_project.expire).getTime()
+        }).subscribe(
+            resp => {
+                this.add_project_msg = resp.message;
+                this.project_list();
+                this.pending_list(true);
+            },
+            err => {
+                console.log('failed to add project', this.new_project);
+                this.add_project_error_msg = err.error.message;
+            }
+        );
     }
 
-    project_list(refresh_requests = false){
+    project_list(refresh_requests = false) {
         this.projects = [];
         this.projectService.list(true).subscribe(
             resp => {
-                if(resp.length == 0) {
+                if (resp.length == 0) {
                     return;
                 }
                 let data = resp;
-                for(var i=0;i<data.length;i++){
+                for (var i = 0; i < data.length; i++) {
                     data[i].expire = new Date(data[i].expire);
                 }
                 this.projects = data;
@@ -225,7 +215,7 @@ export class ProjectsComponent implements OnInit {
                 }
                 let data = resp;
                 if (data.length > 0) { this.requests_visible = true; };
-                this.pending_number= data.length;
+                this.pending_number = data.length;
                 this.pending_projects = data;
             },
             err => console.log('failed to get pending projects')
@@ -233,7 +223,7 @@ export class ProjectsComponent implements OnInit {
 
     }
 
-    date_convert = function timeConverter(tsp){
+    date_convert = function timeConverter(tsp) {
         let res;
         try {
             var a = new Date(tsp);
