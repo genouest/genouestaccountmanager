b3ac8fb5cf2c7cf02df79b4e1c696078c01bc738
KonoganB <konogan.bourhy@gmail.com>
2022-01-26 09:25:46 +0100

    change token

--- core/id.service.js
+++ core/id.service.js
@@ -22,8 +22,8 @@ if (CONFIG.redis !== undefined && CONFIG.redis.host !== undefined && CONFIG.redi
     let redis_cfg = {host: CONFIG.redis.host, port: (CONFIG.redis.port || 6379)};
     logger.info('Using Redis', redis_cfg);
     redis_client = redis.createClient(redis_cfg);
-    redis_client.set(['my:dmp:token', '']);
-    redis_client.set(['my:dmp:expiration', '0']);
+    redis_client.set('my:dmp:token', 'xxx');
+    redis_client.set('my:dmp:expiration', '0');
 } else {
     logger.warn('Using db id mngt, may create issue in case of multi-process!!!');
 }
