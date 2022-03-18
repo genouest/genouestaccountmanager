a934e8e6e6be2156e68c48f8b70041ad8ad363bf
Fanch <francois.gerbes@sb-roscoff.fr>
2022-03-18 08:45:56 +0100

    Generate Better password (#343)
    * add password generatore + npm audit
    
    * generate better password
    
    * add " to unallowed password char
    
    * try to exclude simple quote too
    
    * use simple quote when adding user to prevent shell interpretation
    
    * add & as excluded char

--- templates/ifb/user/add_user.sh
+++ templates/ifb/user/add_user.sh
@@ -2,16 +2,16 @@
 
 set -xe
 
-/usr/local/bin/num create-user --firstname="{{ user.firstname }}" --lastname="{{ user.lastname }}" --email="{{ user.email }}" --username="{{ user.uid }}" {% if user.home %}--home_dir="{{ user.home }}"{% endif %} {% if user.password %}--password="{{ user.password }}"{% endif %} {% if user.is_fake %}--service{% endif %}
+/usr/local/bin/num create-user --firstname='{{ user.firstname }}' --lastname='{{ user.lastname }}' --email='{{ user.email }}' --username='{{ user.uid }}' {% if user.home %}--home_dir='{{ user.home }}'{% endif %} {% if user.password %}--password='{{ user.password }}'{% endif %} {% if user.is_fake %}--service{% endif %}
 
 {% include "user/add_readme.sh" %}
 
-mkdir -p "{{ user.home }}/.ssh"
-touch "{{ user.home }}/.ssh/authorized_keys"
-echo 'Host *' > "{{ user.home }}/.ssh/config"
-echo '  StrictHostKeyChecking no' >> "{{ user.home }}/.ssh/config"
-echo '  UserKnownHostsFile=/dev/null' >> "{{ user.home }}/.ssh/config"
-chown -R {{ user.uid }}:{{ user.uid }} "{{ user.home }}"
+mkdir -p '{{ user.home }}/.ssh'
+touch '{{ user.home }}/.ssh/authorized_keys'
+echo 'Host *' > '{{ user.home }}/.ssh/config'
+echo '  StrictHostKeyChecking no' >> '{{ user.home }}/.ssh/config'
+echo '  UserKnownHostsFile=/dev/null' >> '{{ user.home }}/.ssh/config'
+chown -R {{ user.uid }}:{{ user.uid }} '{{ user.home }}'
 
 {% include "user/add_extra_dirs.sh" %}
 
