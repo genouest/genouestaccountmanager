5562eeed9b534b25856cb0b46d38199e8977efb1
Fanch <fanch@libre.bzh>
2022-03-28 16:08:54 +0200

    protect ldap password with quote as it may contain some shell intepreted char

--- templates/default/user/add_group.sh
+++ templates/default/user/add_group.sh
@@ -4,7 +4,7 @@ echo "Start add_group.sh in $0 ..."
 
 set -e
 
-ldapadd -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ group.name }}.{{ fid }}.ldif"
+ldapadd -h {{ CONFIG.ldap.host }} -cx -w '{{ CONFIG.ldap.admin_password }}' -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ group.name }}.{{ fid }}.ldif"
 
 
 echo "End add_group.sh in $0 ..."
--- templates/default/user/delete_user.sh
+++ templates/default/user/delete_user.sh
@@ -5,9 +5,9 @@ echo "Start delete_user.sh in $0 ..."
 # Allowed to fail as it may already have been deleted
 #set -e
 
-ldapmodify -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
+ldapmodify -h {{ CONFIG.ldap.host }} -cx -w '{{ CONFIG.ldap.admin_password }}' -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
 
-ldapdelete -h {{ CONFIG.ldap.host }} -cx -w {{ CONFIG.ldap.admin_password }} -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn  }} "uid={{ user.uid }},ou=people,{{ CONFIG.ldap.dn }}"
+ldapdelete -h {{ CONFIG.ldap.host }} -cx -w '{{ CONFIG.ldap.admin_password }}' -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn  }} "uid={{ user.uid }},ou=people,{{ CONFIG.ldap.dn }}"
 
 if [ -d "{{ user.home }}" ]
 then
