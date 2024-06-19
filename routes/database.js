3e339248ead50ee6c46f123d402652c220d70e1b
remy siminel <remysben@gmail.com>
2024-06-19 18:15:50 +0200

    fix

--- routes/database.js
+++ routes/database.js
@@ -12,82 +12,8 @@ const udbsrv = require('../core/user_db.service.js');
 const sansrv = require('../core/sanitize.service.js');
 const rolsrv = require('../core/role.service.js');
 
-const mysql = require('mysql');
 const winston = require('winston');
 const logger = winston.loggers.get('gomngr');
-// const request = require('request');
-
-
-var pool = null;
-if(CONFIG.mysql.host) {
-    pool = mysql.createPool({
-        host     : CONFIG.mysql.host,
-        user     : CONFIG.mysql.user,
-        password : CONFIG.mysql.password,
-        insecureAuth: true
-    });
-}
-
-/*
-var connection;
-
-function handleDisconnect() {
-    if (CONFIG.mysql.host) {
-        connection = mysql.createConnection({
-            host     : CONFIG.mysql.host,
-            user     : CONFIG.mysql.user,
-            password : CONFIG.mysql.password,
-            insecureAuth: true
-        }); // Recreate the connection, since
-        // the old one cannot be reused.
-
-        connection.connect(function(err) {              // The server is either down
-            if(err) {                                     // or restarting (takes a while sometimes).
-                logger.error('error when connecting to db:', err);
-                setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-            }                                     // to avoid a hot loop, and to allow our node script to
-        });                                     // process asynchronous requests in the meantime.
-        // If you're also serving http, display a 503 error.
-        connection.on('error', function(err) {
-            logger.error('db error', err);
-            if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-                handleDisconnect();                         // lost due to either server restart, or a
-            } else {                                      // connnection idle timeout (the wait_timeout
-                // throw err;                                  // server variable configures this)
-            }
-        });
-    }
-}
-
-handleDisconnect();
-*/
-
-var querydb = (sql) => {
-    return new Promise((resolve, reject) => {
-        if(pool === null) {
-            reject('no mysql pool defined');
-            return;
-        }
-        pool.getConnection((err, connection) => {
-            if(err) {
-                logger.error('failed to get connection', err);
-                reject(err);
-                return;
-            }
-            // eslint-disable-next-line no-unused-vars
-            connection.query(sql, function(err, results) {
-                connection.release();
-                if(err) {
-                    logger.error('failed to exec sql', sql, err);
-                    reject(err);
-                    return;
-                }
-                logger.debug('sql result', sql, results);
-                resolve(true);
-            });
-        });
-    });
-};
 
 
 /**
@@ -398,7 +324,7 @@ router.delete('/database/:id', async function(req, res) {
         return;
     }
     try {
-        await udbsrv.delete_db(req.params.id, session_user.uid, is_admin)
+        await udbsrv.delete_db(req.params.id, session_user.uid, is_admin);
         res.send({ message: 'Database removed' });
     } catch (e) {
         logger.error(e);
