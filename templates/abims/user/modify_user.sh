f8c8e27f60fce53cd644863423bfafc6b70fe879
Gildas Le Corguillé <lecorguille@users.noreply.github.com>
2024-09-27 17:20:59 +0200

    Change ldapmodify usage since ubuntu22 (#516)

--- templates/abims/user/modify_user.sh
+++ templates/abims/user/modify_user.sh
@@ -4,6 +4,6 @@ echo "Start modify_user.sh in $0 ..."
 
 set -e
 
-ldapmodify -h {{ CONFIG.ldap.host }} -cx -w '{{ CONFIG.ldap.admin_password }}' -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
+ldapmodify -H ldap://{{ CONFIG.ldap.host }} -cx -w '{{ CONFIG.ldap.admin_password }}' -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
 
 echo "End modify_user.sh in $0 ..."
