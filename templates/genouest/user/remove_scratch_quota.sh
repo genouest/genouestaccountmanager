eeca4cf89878918fb731745a5914634500acd93a
mboudet <mateo.boudet@gmail.com>
2024-12-04 17:33:33 +0100

    Update remove_scratch_quota.sh

--- templates/genouest/user/remove_scratch_quota.sh
+++ templates/genouest/user/remove_scratch_quota.sh
@@ -4,11 +4,7 @@ curl -Lk '{{ CONFIG.custom_users.quota.url }}' \
 --header "Content-Type: application/json" \
 --request DELETE \
 -u '{{ CONFIG.custom_users.quota.user }}:{{ CONFIG.custom_users.quota.password }}' \
---data \
-'
-{
-    "path": "{{ CONFIG.custom_users.quota.base_path }}/{{ user.uid }}"
-}
-'
+-G \
+--data  'type=directory&path={{ CONFIG.custom_users.quota.base_path }}/{{ user.uid }}'
 
 echo "End remove_scratch_quota.sh in $0 ..."
