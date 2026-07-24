8a3692ef101f2229dc3b0c9a96ca85b23cc7cee1
mboudet <mateo.boudet@gmail.com>
2026-07-24 15:46:03 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -777,7 +777,7 @@ router.post('/user/:id', async function (req, res) {
         history: [{ action: 'register', date: new Date().getTime() }],
         extra_info: req.body.extra_info || [],
         registration: new Date().getTime(),
-        disable_extend: CONFIG.general.default_disable_extend
+        disable_extend: CONFIG.general.default_disable_extend || false
     };
 
     // check if register user is done by admin or by anonymouse user
