e5747a0ab5276902d7b8ac7588f6afb5c52cc789
Mathias Hebras <mathias.hebras@irisa.fr>
2026-07-31 14:14:53 +0200

    fix(upgrade images): Adapt templates for ldap-utils compatibility
    - The new ldap-utils versions disable the usage of ldapadd -h and replace it by ldapadd -H. This commit adapt the templates to use the new argument.
    - Facilitates local usage of the repo for dev and test by allowing the usage of newer ldap-utils versions.

--- templates/default/user/expire_user.sh
+++ templates/default/user/expire_user.sh
@@ -4,7 +4,7 @@ echo "Start expire_user.sh in $0 ..."
 
 set -e
 
-ldapmodify -h {{ CONFIG.ldap.host }} -cx -w '{{ CONFIG.ldap.admin_password }}' -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
+ldapmodify -H ldap://{{ CONFIG.ldap.host }} -cx -w '{{ CONFIG.ldap.admin_password }}' -D {{ CONFIG.ldap.admin_cn }},{{ CONFIG.ldap.admin_dn }} -f "{{ CONFIG.general.script_dir }}/{{ user.uid }}.{{ fid }}.ldif"
 
 if [ -e "{{ user.home }}/.ssh/authorized_keys" ]; then
     mv  "{{ user.home }}/.ssh/authorized_keys" "{{ user.home }}/.ssh/authorized_keys.expired"
